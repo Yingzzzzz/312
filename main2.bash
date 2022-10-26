d6c93ba3bd9f997cbafe07926644bda93ce36eda
Yingzzzzz <96532087+Yingzzzzz@users.noreply.github.com>
2022-10-26 10:00:33 +0300

    Update main2.bash

--- main2.bash
+++ main2.bash
@@ -3,11 +3,10 @@ cd web_monitor_log
 touch Web_log.csv  #Final output
 touch WebLog.csv
 
-wget https://raw.githubusercontent.com/Yingzzzzz/76/main/directory.bash
+wget https://raw.githubusercontent.com/Yingzzzzz/312/main/directory.bash
 chmod +x directory.bash
- ./directory.bash & PID=$!
-  sleep 2 
- kill $PID
+ ./directory.bash
+
  
 #duplicate
 awk -F '|' '!x[$1]++' WebLog.csv > Web_Log.csv
